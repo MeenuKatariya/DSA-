fafc000af230752690855a53de47a9e17067a683
Meenu Katariya <katariyameenu963@gmail.com>
2024-01-02 23:24:09 +0530

    bubble sort

--- sortarr.js
+++ sortarr.js
@@ -31,3 +31,19 @@
         arr[a] = arr[b];
         arr[b] = temp;
     }
+
+    // Bubble Sort
+
+    for(let i=0;i<arr.length-1;i++){
+        for(let j= 0; j<arr.length-i-1; j++){
+            if(arr[j]>arr[j+1]){
+                swap(arr, j ,j+1);
+
+            }
+        }
+    }
+    function swap(arr,a,b){
+        let temp = arr[a];
+        arr[a] = arr[b];
+        arr[b] = temp;
+    }
