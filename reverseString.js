ba77f8f9890fcbe99bc20c1226e86ed1cfa20d6f
Meenu Katariya <katariyameenu963@gmail.com>
2024-01-02 12:48:09 +0530

    reverse-string-firstMethod

---
+++ reverseString.js
@@ -0,0 +1,13 @@
+// Input:
+// s = Geeks
+// Output: skeeG
+
+// First Method
+
+class Solution {
+    
+    reverseWord(str){
+      let string = str.split("").reverse().join("");
+       return string;
+    }
+}
